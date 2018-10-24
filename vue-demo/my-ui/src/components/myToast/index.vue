3f14676cf0f83e788b912f8a5f89dc9eaeba8308
wangkaiwd <1162486176@qq.com>
2018-10-24 22:40:48 +0800

    脚手架初始化项目

---
+++ vue-demo/my-ui/src/components/myToast/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="my-toast">
+    MyToast
+  </div>
+</template>
+
+<script>
+export default {
+	name: 'MyToast',
+	props: {},
+	mounted() {}
+}
+</script>
+
+<style lang="less" scoped>
+</style>
